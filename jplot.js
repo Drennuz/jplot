14e2ec47315c2d044e28ed0cd6f863423a587760
Drennuz <xiayunsun@gmail.com>
2013-06-06 15:03:12 -0400

    a bit cleansing...

--- jplot.js
+++ jplot.js
@@ -5,11 +5,13 @@
     var FONT_TYPE = 'Arial'; 
     var FONT_SHRINK = 0.6;
     var USAGE = "dataset format: [[data labels], [dataset1], [dataset2], ...]. Data labels and dataset*s must be of same length"
+    var OFFSET = 0.1;
 
     //checking if all elements in one array are of the same length
     var checkEqualSize = function (array){
-        var eleLength = array.map(function(x){return x.length;}); 
-        for (var i = 0; i < eleLength.length - 1; i++){
+        var eleLength, i;
+        eleLength = array.map(function(x){return x.length;}); 
+        for (i = 0; i < eleLength.length - 1; i++){
             if (eleLength[i] != eleLength[i+1])
                 return false;
         }
@@ -25,11 +27,23 @@
     //return suitable font size for the specified area
     var resizeText = function (canvasContext, text, areaWidth){
         var baseSize = 300;
+        var textWidth, factor;
+
         canvasContext.font = baseSize + "px " + FONT_TYPE; //base
-        var textWidth = canvasContext.measureText(text).width;
-        var factor = areaWidth / textWidth;
+        textWidth = canvasContext.measureText(text).width;
+        factor = areaWidth / textWidth;
         baseSize = Math.round(FONT_SHRINK * baseSize * factor); // only take 60%
         return baseSize;
+    }
+
+    //plot main title
+    var plotMain = function (canvas, text){
+        var context = canvas.getContext('2d');
+        fontSize = resizeText(context, text, (1-2*OFFSET)*canvas.width);
+        context.textAlign = "center";
+        context.font = fontSize + "px " + FONT_TYPE;
+        textWidth = context.measureText(text).width;
+        context.fillText(text, 0.5*canvas.width, 0.8 * OFFSET * canvas.height); 
     }
 
     //***** plotting starts here *****
@@ -40,7 +54,6 @@
         var context = canvas.getContext('2d');
         var options = optionalArgs || {};
         
-        // consider use using options object and not storing in separate `var`s
         if(options.beside == undefined)
             var beside = true;
         else
@@ -52,55 +65,58 @@
         //checking dataset: length >= 2; all elements have equal length
         if (dataset.length <= 1 || (!checkEqualSize(dataset))) throw new Error(USAGE);
         //draw
-        var offset = 0.1; //take 80% of total area for bars
+        var maxHeight, singleBarWidth, singleBarHeight, baseX, baseY, startX, startY;
+        var colorCode, colorStyle;
+        var i,j;
+
         var barGroupCount = dataset[0].length;
         var realData = dataset.slice(1);
         var dataSetCount = realData.length;
         var longestLabel = maxElement(dataset[0], function (x){return x.length;}); 
-        var fontSize = FONT_SHRINK * resizeText(context, longestLabel, (1-2*offset)*canvas.width/barGroupCount);
+        var fontSize = FONT_SHRINK * resizeText(context, longestLabel, (1-2*OFFSET)*canvas.width/barGroupCount);
         
         if (beside){ //juxtaposed
-            var maxHeight = maxElement(realData, function (x){return x;});
-            var singleBarWidth = Math.round((1-2*offset)*canvas.width/barGroupCount*(1-offset)/dataSetCount);
-            for (var i = 0; i < barGroupCount; i++){
-                var baseX = Math.round(2 * offset * canvas.width + i * (1 - 2*offset) * canvas.width / barGroupCount);
-                var baseY = Math.round(offset * canvas.height);
+            maxHeight = maxElement(realData, function (x){return x;});
+            singleBarWidth = Math.round((1-2*OFFSET)*canvas.width/barGroupCount*(1-OFFSET)/dataSetCount);
+            for (i = 0; i < barGroupCount; i++){
+                baseX = Math.round(2 * OFFSET * canvas.width + i * (1 - 2*OFFSET) * canvas.width / barGroupCount);
+                baseY = Math.round(OFFSET * canvas.height);
                 
-                for (var j = 0; j < dataSetCount; j++){
-                    var colorCode = Math.round(0 + j * 255 / dataSetCount);
-                    var colorStyle = "rgb(" + ([colorCode,colorCode,colorCode]).join(',') + ")";
+                for (j = 0; j < dataSetCount; j++){
+                    colorCode = Math.round(0 + j * 255 / dataSetCount);
+                    colorStyle = "rgb(" + ([colorCode,colorCode,colorCode]).join(',') + ")";
                     context.fillStyle = colorStyle;
-                    var singleBarHeight = Math.round((1-2*offset)*canvas.height*realData[j][i]/maxHeight);
-                    var startY = baseY + Math.round((1-2*offset)*canvas.height - singleBarHeight);
-                    var startX = baseX + j * singleBarWidth;
+                    singleBarHeight = Math.round((1-2*OFFSET)*canvas.height*realData[j][i]/maxHeight);
+                    startY = baseY + Math.round((1-2*OFFSET)*canvas.height - singleBarHeight);
+                    startX = baseX + j * singleBarWidth;
                     context.fillRect(startX, startY, singleBarWidth, singleBarHeight);
                 }
                 //adding x-axis labels
                 context.textAlign = "left";
                 context.fillStyle = "rgb(0,0,0)";
                 context.font = 0.8 * fontSize + "px " + FONT_TYPE;
-                context.fillText(dataset[0][i], baseX, Math.round((1 - 0.5*offset)*canvas.height));
+                context.fillText(dataset[0][i], baseX, Math.round((1 - 0.5*OFFSET)*canvas.height));
             }
         }
         else{ //stacked
             var sumHeights = [];
-            var singleBarWidth = Math.round((1-2*offset)*canvas.width/barGroupCount*(1-offset));
-            for (var i = 0; i < barGroupCount; i++){
+            singleBarWidth = Math.round((1-2*OFFSET)*canvas.width/barGroupCount*(1-OFFSET));
+            for (i = 0; i < barGroupCount; i++){
                 var temp = 0;
-                for (var j = 0; j < dataSetCount; j++){
+                for (j = 0; j < dataSetCount; j++){
                     temp += realData[j][i];
                 }
                 sumHeights.push(temp);
             }
-            var maxHeight = maxElement(sumHeights, function(x){return x;});
+            maxHeight = maxElement(sumHeights, function(x){return x;});
             for (i = 0; i < barGroupCount; i++){
-                var baseX = Math.round(2 * offset * canvas.width + i * (1 - 2*offset) * canvas.width/barGroupCount);
-                var baseY = Math.round((1-offset) * canvas.height);
+                baseX = Math.round(2 * OFFSET * canvas.width + i * (1 - 2*OFFSET) * canvas.width/barGroupCount);
+                baseY = Math.round((1-OFFSET) * canvas.height);
                 for (j = 0; j < dataSetCount; j++){
-                    var colorCode = Math.round(0 + j * 255 / dataSetCount);
-                    var colorStyle = "rgb(" + ([colorCode, colorCode, colorCode]).join(',') + ")";
+                    colorCode = Math.round(0 + j * 255 / dataSetCount);
+                    colorStyle = "rgb(" + ([colorCode, colorCode, colorCode]).join(',') + ")";
                     context.fillStyle = colorStyle;
-                    var singleBarHeight = Math.round((1-2*offset)*canvas.height*realData[j][i]/maxHeight);
+                    singleBarHeight = Math.round((1-2*OFFSET)*canvas.height*realData[j][i]/maxHeight);
                     baseY -= singleBarHeight;
                     context.fillRect(baseX, baseY, singleBarWidth, singleBarHeight);
                 }
@@ -108,54 +124,48 @@
                 context.textAlign = "left";
                 context.fillStyle = "rgb(0,0,0)";
                 context.font = 0.8 * fontSize + "px " + FONT_TYPE;
-                context.fillText(dataset[0][i], baseX, Math.round((1 - 0.5*offset)*canvas.height));
+                context.fillText(dataset[0][i], baseX, Math.round((1 - 0.5*OFFSET)*canvas.height));
             }
         }
 
         //adding y-axis measurements
-        startX = Math.round((2-offset) * offset * canvas.width);
-        context.moveTo(startX, Math.round(offset * canvas.height));
-        context.lineTo(startX, Math.round((1-offset) * canvas.height));
+        startX = Math.round((2-OFFSET) * OFFSET * canvas.width);
+        context.moveTo(startX, Math.round(OFFSET * canvas.height));
+        context.lineTo(startX, Math.round((1-OFFSET) * canvas.height));
         context.stroke();
 
         var intervals = Math.round(maxHeight / 5);
         context.fillStyle = "rgb(0,0,0)";
-        fontSize = Math.round(0.8 * resizeText(context, maxHeight, (1-offset)*offset*canvas.width));
+        fontSize = Math.round(0.8 * resizeText(context, maxHeight, (1-OFFSET)*OFFSET*canvas.width));
         context.textAlign = "right";
         context.font = fontSize + "px " + FONT_TYPE;
         for (i = 0; i < maxHeight; i = i + intervals){
-            startY = (1-offset)*canvas.height - (i/maxHeight) * (1-2*offset) * canvas.height;
-            context.fillText(i, 2 * (1 - offset) * offset*canvas.width, startY); 
+            startY = (1-OFFSET)*canvas.height - (i/maxHeight) * (1-2*OFFSET) * canvas.height;
+            context.fillText(i, 2 * (1 - OFFSET) * OFFSET*canvas.width, startY); 
         }
         //adding maxHeight
-        context.fillText(maxHeight, 2 * (1 - offset) * offset * canvas.width, offset * canvas.height);
-
+        context.fillText(maxHeight, 2 * (1 - OFFSET) * OFFSET * canvas.width, OFFSET * canvas.height);
 
         //adding other axis titles
         context.fillStyle = "rgb(0,0,0)";
         if(ylab){
             context.save();
-            context.translate(offset * canvas.width, (1 - offset) * canvas.height);
+            context.translate(OFFSET * canvas.width, (1 - OFFSET) * canvas.height);
             context.rotate(-Math.PI/2.0);
             context.font = fontSize + "px " + FONT_TYPE;
             context.textAlign = "center";
-            context.fillText(ylab, (0.5 - offset) * canvas.height, 0);
+            context.fillText(ylab, (0.5 - OFFSET) * canvas.height, 0);
             context.restore();
         }
         if(xlab){
             context.font = fontSize + "px " + FONT_TYPE;
             context.textAlign = "center";
-            context.fillText(xlab, 0.5 * (1+offset) * canvas.width, canvas.height);
+            context.fillText(xlab, 0.5 * (1+OFFSET) * canvas.width, canvas.height);
         }
 
         if(main){
-            fontSize = resizeText(context, main, (1-2*offset)*canvas.width);
-            context.textAlign = "center";
-            context.font = fontSize + "px " + FONT_TYPE;
-            textWidth = context.measureText(main).width;
-            context.fillText(main, 0.5*canvas.width, 0.8 * offset * canvas.height); 
+            plotMain(canvas, main);
         }
-
     }
     var handleDOMContentLoaded = function (e){
         var canvases = document.getElementsByClassName('jplot');
